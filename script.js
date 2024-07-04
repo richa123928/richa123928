df6bcbf7bd8591fd5f588c8ac31ac9635155eaaf
richa123928 <yricha246@gmail.com>
2024-07-04 09:46:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,63 @@
-var ms = 0, s = 0, m = 0, h = 0
-var timer;
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
 
-var display = document.querySelector(".timer-Display")
-var laps = document.querySelector(".laps")
+const location_not_found = document.querySelector('.location-not-found');
+
+const weather_body = document.querySelector('.weather-body');
 
 
-function start(){
-    if(!timer){
-        timer = setInterval(run, 10)
+async function checkWeather(city){
+    const api_key = "4cd0eee81294c867b4bc4cfc64e998c5";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
     }
-}
 
-function run(){
-    display.innerHTML = getTimer()
-    ms++              
-    if(ms == 100){
-        ms = 0
-        s++
+    console.log("run");
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+            weather_img.src = "/assets/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "/assets/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "/assets/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "/assets/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "/assets/snow.png";
+            break;
+
     }
-    if(s == 60){
-        s = 0
-        m++
-    }
-    if(m == 60){
-        m = 0
-        h++
-    }
-    
-    if(h == 13){
-        h = 1
-    } 
-}
 
-function getTimer(){
-    return (h<10 ? "0" + h: h) + " : " + (m<10 ? "0" + m : m) + " : " + (s<10 ? "0" + s : s) + " : " + (ms<10 ? "0" + ms : ms); 
+    console.log(weather_data);
 }
 
 
-
-function pause(){
-    stopTimer()  
-}
-
-function stopTimer(){
-    clearInterval(timer)
-    timer = false 
-}
-
-
-
-
-function reset(){
-    stopTimer()
-    ms = 0
-    s = 0
-    m = 0
-    h = 0
-    display.innerHTML = getTimer()
-}
-
-
-
-
-function restart(){
-    if(timer){ 
-        reset()
-        start()
-    }
-    
-}
-
-
-// lap = taking screenshot of current time...
-function lap() {
-    if(timer) {   
-        var Li = document.createElement("li")   
-        Li.innerHTML = getTimer() 
-        laps.appendChild(Li) 
-    }
-}
-
-function resetLap(){
-    laps.innerHTML = ""
-}
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
